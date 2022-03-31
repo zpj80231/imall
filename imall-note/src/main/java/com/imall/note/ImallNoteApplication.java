1c499187afee2fe8ac7681432e8341eaf27dd7ed
zpj80231 <zpj80231@163.com>
2022-03-31 18:23:44 +0800

    feat(imall-note): add imall-note

---
+++ imall-note/src/main/java/com/imall/note/ImallNoteApplication.java
@@ -0,0 +1,13 @@
+package com.imall.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImallNoteApplication.class, args);
+    }
+
+}
---
+++ imall-note/src/test/java/com/imall/note/ImallNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imall.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImallNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
