220e9a407194752928b2f5337c843da0fbb6e21a
zpj80231 <zpj80231@163.com>
2022-07-13 09:24:16 +0800

    refactor(imall-common): update CommonPage.java

--- imall-common/src/main/java/com/imall/common/api/CommonPage.java
+++ imall-common/src/main/java/com/imall/common/api/CommonPage.java
@@ -1,5 +1,6 @@
 package com.imall.common.api;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 
@@ -25,7 +26,7 @@ public class CommonPage<T> {
     /** 当前页的数据 */
     private List<T> list;
 
-    public static <T> CommonPage<T> restPage(Page<T> pageResult) {
+    public static <T> CommonPage<T> restPage(IPage<T> pageResult) {
         CommonPage<T> result = new CommonPage<>();
         result.setPageNum(pageResult.getCurrent());
         result.setPageSize(pageResult.getSize());
@@ -33,5 +34,21 @@ public class CommonPage<T> {
         result.setTotalPage(pageResult.getTotal()/pageResult.getSize()+1);
         result.setList(pageResult.getRecords());
         return result;
+    }
+
+    public static <T> CommonPage<T> restPage(IPage<?> pageResult, List<T> list) {
+        CommonPage<T> result = new CommonPage<>();
+        result.setPageNum(pageResult.getCurrent());
+        result.setPageSize(pageResult.getSize());
+        result.setTotal(pageResult.getTotal());
+        result.setTotalPage(pageResult.getTotal()/pageResult.getSize()+1);
+        result.setList(list);
+        return result;
+    }
+
+    public static <T> Page<T> getPage(Integer currPage, Integer pageSize) {
+        currPage = currPage == null || currPage == 0 ? 1 : currPage;
+        pageSize = pageSize == null || pageSize == 0 ? 10 : pageSize;
+        return new Page<>(currPage, pageSize);
     }
 }
