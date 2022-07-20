56cc5148676a51708063d7ea5d705274eb4309d7
zpj80231 <zpj80231@163.com>
2022-07-20 16:58:35 +0800

    解决统一返回类型为String时转换错误

---
+++ imall-common/src/main/java/com/imall/common/config/HttpMessageConvertersConfig.java
@@ -0,0 +1,30 @@
+package com.imall.common.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * 解决统一返回类型为String时转换错误
+ * @author zhangpengjun
+ * @date 2022/7/15
+ */
+@Configuration
+public class HttpMessageConvertersConfig {
+
+    @Bean
+    public HttpMessageConverters custHttpMessageConverter() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd hh:mm:ss");
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+
+}
