0afd80ee8cc883fc239af75ad8806dadf50f7068
zpj80231 <zpj80231@163.com>
2022-07-27 17:15:03 +0800

    优化包依赖和升级swagger

--- imall-mbg/src/main/java/com/imall/mbg/config/Swagger2Config.java
+++ imall-mbg/src/main/java/com/imall/mbg/config/Swagger2Config.java
@@ -6,39 +6,39 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @ClassName SwaggerConfig
- * @Description 采用 knife4j 美化 swagger
- *              https://knife4j.xiaominfo.com/
+ * @ClassName Swagger2Config
+ * @Description 采用 knife4j 美化swagger
  * @Version 1.0
  * @Date 2020/9/29 22:59
  * @Created by zpj80231
  */
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class Swagger2Config {
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .groupName("IMall Common")
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.imall.mbg.modules.ums"))
+                .apis(RequestHandlerSelectors.basePackage("com.imall.mbg.modules"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("imall")
-                .description("imall API 文档")
-                // swagger地址，/swagger-ui.html
-                //.termsOfServiceUrl("http://localhost:8080/swagger-ui.html")
-                // 美化后地址，doc.html
-                .termsOfServiceUrl("http://localhost:8080/doc.html")
+                .title("IMall")
+                .description("IMall API 文档")
+                .termsOfServiceUrl("http://localhost:8000/doc.html")
+                .contact(new Contact("IMall", "https://imall.com", "zpj80231@163.com"))
                 .version("1.0")
                 .build();
     }
