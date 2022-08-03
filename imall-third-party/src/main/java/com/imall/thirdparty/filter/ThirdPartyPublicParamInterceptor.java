7540c980b3f660867f8448128c071ab0819d8ad1
zpj80231 <zpj80231@163.com>
2022-08-03 11:00:04 +0800

    feat(imall-third-party): 更新命名

--- imall-third-party/src/main/java/com/imall/thirdparty/filter/ThirdPartyPublicParamInterceptor.java
+++ imall-third-party/src/main/java/com/imall/thirdparty/filter/ThirdPartyPublicParamInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * DkyThirdPartyParamInterceptor
+ * ThirdPartyPublicParamInterceptor
  *
  * @author zhangpengjun
  * @date 2022/7/12
