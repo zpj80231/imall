c27211c7b34291841d0dc4762a92ae6f1b65c64a
zpj80231 <zpj80231@163.com>
2022-12-22 10:46:34 +0800

    refactor(admin): 添加日志

--- imall-admin/src/main/java/com/imall/admin/component/RestAuthenticationEntryPoint.java
+++ imall-admin/src/main/java/com/imall/admin/component/RestAuthenticationEntryPoint.java
@@ -1,7 +1,9 @@
 package com.imall.admin.component;
 
+import cn.hutool.json.JSON;
 import cn.hutool.json.JSONUtil;
 import com.imall.common.api.CommonResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -16,16 +18,19 @@ import java.io.IOException;
  * @author zhangpengjun
  * @date 2022/8/24
  */
+@Slf4j
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+        JSON msg = JSONUtil.parse(CommonResult.unauthorized(authException.getMessage()));
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Cache-Control", "no-cache");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
-        response.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(authException.getMessage())));
+        response.getWriter().println(msg);
         response.getWriter().flush();
+        log.info("身份认证异常: {}", msg);
     }
 }
