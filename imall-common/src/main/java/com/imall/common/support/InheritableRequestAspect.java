c94a14753acef0d4d02e2dd967440b0ee0ab656b
zpj80231 <zpj80231@163.com>
2023-04-03 15:38:59 +0800

    refactor(imall-common): 使 HttpServletRequest 可以继续在异步线程中使用

--- imall-common/src/main/java/com/imall/common/support/InheritableRequestAspect.java
+++ imall-common/src/main/java/com/imall/common/support/InheritableRequestAspect.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 /**
  * 主要用于在 @Async 使用 Request
  * 使用 @Async 获取 Request 时，内部方法不能用 this（this是本身而不是代理对象） 调用，否则无效。
+ * 也可以在线程池包装处处理：参考 {@link ThreadPoolExecutorMdc#inheritableRequest()}，此处则是单独抽离，按需引入。
  *
  * @author zhangpengjun
  * @date 2023/3/31
@@ -23,7 +24,7 @@ public class InheritableRequestAspect {
 
     // 直接拦截Async注解后，在异步方法中获取request时，有时生效有时不生效，可能是个bug
     // @Pointcut("@annotation(org.springframework.scheduling.annotation.Async)")
-    // 推荐：直接在controller拦截的话就没问题
+    // 推荐：直接在 controller 拦截的话就没问题
     @Pointcut("execution(* com.imall..controller..*.*(..))")
     public void car() {
 
--- imall-common/src/main/java/com/imall/common/support/ThreadPoolExecutorMdc.java
+++ imall-common/src/main/java/com/imall/common/support/ThreadPoolExecutorMdc.java
@@ -2,6 +2,8 @@ package com.imall.common.support;
 
 import org.slf4j.MDC;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
@@ -18,16 +20,33 @@ public class ThreadPoolExecutorMdc extends ThreadPoolTaskExecutor {
 
     @Override
     public void execute(Runnable task) {
+        inheritableRequest();
         super.execute(ThreadPoolMDCFilter.wrap(task, MDC.getCopyOfContextMap()));
     }
 
+    /**
+     * 提交
+     *
+     * @param task 任务
+     * @return {@link Future}<{@link T}>
+     */
     @Override
     public <T> Future<T> submit(Callable<T> task) {
+        inheritableRequest();
         return super.submit(ThreadPoolMDCFilter.wrap(task, MDC.getCopyOfContextMap()));
     }
 
     @Override
     public Future<?> submit(Runnable task) {
+        inheritableRequest();
         return super.submit(ThreadPoolMDCFilter.wrap(task, MDC.getCopyOfContextMap()));
+    }
+
+    /**
+     * 在异步线程中，使 HttpServletRequest 可以继续在异步线程中使用
+     */
+    public void inheritableRequest() {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        RequestContextHolder.setRequestAttributes(servletRequestAttributes, true);
     }
 }
