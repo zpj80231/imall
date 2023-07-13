4faee092c123fc6b229b5769a66a45117e01c317
zpj80231 <zpj80231@163.com>
2023-07-13 17:37:23 +0800

    refactor(imall-common): 更新默认异步配置

--- imall-common/src/main/java/com/imall/common/config/BeseAsyncConfig.java
+++ imall-common/src/main/java/com/imall/common/config/BeseAsyncConfig.java
@@ -1,13 +1,14 @@
 package com.imall.common.config;
 
-import com.imall.common.constant.ThreadPoolConstant;
 import com.imall.common.support.ThreadPoolExecutorMdc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 /**
  * @author zhangpengjun
@@ -16,9 +17,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Slf4j
 @EnableAsync
 @Configuration
-public class BeseAsyncConfig {
+public class BeseAsyncConfig implements AsyncConfigurer {
 
-    @Value("${spring.task.execution.pool.core-size:2}")
+    @Value("${spring.task.execution.pool.core-size:4}")
     private int corePoolSize;
     @Value("${spring.task.execution.pool.max-size:20}")
     private int maxPoolSize;
@@ -26,18 +27,34 @@ public class BeseAsyncConfig {
     private int keepAliveSeconds;
     @Value("${spring.task.execution.pool.queue-capacity:2000}")
     private int queueCapacity;
-    @Value("${spring.task.execution.thread-name-prefix:sync-exec-}")
+    @Value("${spring.task.execution.thread-name-prefix:async-exec-}")
     private String threadNamePrefix;
 
-    @Bean(name = ThreadPoolConstant.ASYNC_DEFAULT_POOL)
-    public ThreadPoolTaskExecutor asyncExecutor() {
-        ThreadPoolExecutorMdc httpSendPool = new ThreadPoolExecutorMdc();
-        httpSendPool.setCorePoolSize(corePoolSize);
-        httpSendPool.setMaxPoolSize(maxPoolSize);
-        httpSendPool.setKeepAliveSeconds(keepAliveSeconds);
-        httpSendPool.setQueueCapacity(queueCapacity);
-        httpSendPool.setThreadNamePrefix(threadNamePrefix);
-        return httpSendPool;
+    @Override
+    public Executor getAsyncExecutor() {
+        return createThreadPoolTaskExecutor(corePoolSize, maxPoolSize, keepAliveSeconds, queueCapacity, threadNamePrefix);
+    }
+
+    /**
+     * 创建线程池执行器
+     *
+     * @param corePoolSize     核心池大小
+     * @param maxPoolSize      最大池大小
+     * @param keepAliveSeconds 维持几秒钟
+     * @param queueCapacity    队列容量
+     * @param threadNamePrefix 线程名称前缀
+     * @return {@link ThreadPoolTaskExecutor}
+     */
+    public ThreadPoolTaskExecutor createThreadPoolTaskExecutor(int corePoolSize, int maxPoolSize, int keepAliveSeconds,
+                                                               int queueCapacity, String threadNamePrefix) {
+        ThreadPoolExecutorMdc threadPoolExecutorMdc = new ThreadPoolExecutorMdc();
+        threadPoolExecutorMdc.setCorePoolSize(corePoolSize);
+        threadPoolExecutorMdc.setMaxPoolSize(maxPoolSize);
+        threadPoolExecutorMdc.setKeepAliveSeconds(keepAliveSeconds);
+        threadPoolExecutorMdc.setQueueCapacity(queueCapacity);
+        threadPoolExecutorMdc.setThreadNamePrefix(threadNamePrefix);
+        threadPoolExecutorMdc.initialize();
+        return threadPoolExecutorMdc;
     }
 
 }
